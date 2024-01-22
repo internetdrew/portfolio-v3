82004821a022fa9d7a983221bb8a2f0fa1457d82
Andrew Rowley <andrew@internetdrew.com>
2024-01-22 13:46:02 -0500

    remove packages for later implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import rehypeHighlight from "rehype-highlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  markdown: {
-    rehypePlugins: [rehypeHighlight],
-  },
 });
