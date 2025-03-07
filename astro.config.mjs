cdc7bbe41db5aa968e626c17d4ba7ff8010db2ed
Andrew Rowley <andrew@internetdrew.com>
2025-03-07 00:49:53 -0500

    add trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.internetdrew.com",
+  site: "https://www.internetdrew.com/",
   integrations: [tailwind(), react(), sitemap()],
   markdown: {
     remarkPlugins: [remarkToc],
