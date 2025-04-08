1828fa471661e79ea35873ed4e3e6b56292b64b0
Andrew Rowley <andrew@internetdrew.com>
2025-04-08 11:13:16 -0400

    extend description

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I build websites and apps that bring brands to life, improve how they serve customers, and simplify daily operations.";
+  "I build websites and apps that bring brands to life, improve how they serve customers, and simplify daily operations — blending thoughtful design with creative storytelling across digital platforms.";
