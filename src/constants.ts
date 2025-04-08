6a996cd8d7fb1f8da20ede25190e9dad9e549ed8
Andrew Rowley <andrew@internetdrew.com>
2025-04-08 12:16:09 -0400

    improve description copy

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I build digital experiences that turn brands into stories people want to follow across the web, in products, and on social.";
+  "I build digital experiences that turn brands into stories people want to follow across the web, through products, and on social.";
