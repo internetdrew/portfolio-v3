728be6f80a088f522621b4d689f549ea7a431813
Andrew Rowley <andrew@internetdrew.com>
2025-04-16 12:31:46 -0400

    update description

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I build fast, beautiful websites, develop custom web apps with clean, intuitive interfaces, and help brands tell scroll-stopping visual stories on social media.";
+  "I build fast, beautiful websites, develop web apps with intuitive interfaces, and help brands tell visual stories on the internet.";
