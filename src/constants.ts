70b81fad4a0d559c36fb6407dcd616435d482f93
Andrew Rowley <95974309+internetdrew@users.noreply.github.com>
2025-06-06 14:27:50 -0400

    Item display (#12)
    * update description and add backup options
    
    * add protected outlet explanation
    
    * add screenshots
    
    * add context for the ui display and shopping list population
    
    * add new screenshots
    
    * switch to relative import paths
    
    * add link to memo
    
    * switch to last updated date
    
    * switch date preference on list
    
    * change heading title

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,13 @@
 export const siteDescription =
-  "I build tools that make managing the mess behind the work way easier.";
+  "I build software that brings structure and visibility to the chaos behind operations.";
+
+const otherOptions = [
+  "I make hard-to-manage systems feel intuitive.",
+  "I translate tangled workflows into tools people actually enjoy using.",
+  "I build software that brings structure and visibility to the mess behind the work.",
+  "I build software that makes managing the messy parts of work feel simple.",
+  "I build software that brings structure and visibility to the chaos behind operations.",
+  "I build software that brings clarity to the moving parts behind your business.",
+  "I help creative agencies build systems that make their ops feel as clean as their final deliverables.",
+  "I help content agencies streamline messy workflows—so their team can focus on creating, not chasing.",
+];
