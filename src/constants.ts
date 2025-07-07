026a64ecae9d4d6acc7bae5b90e52d237968062e
Andrew Rowley <andrew@internetdrew.com>
2025-07-07 14:52:09 -0400

    update desc

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I bridge engineering and marketing teams, building tools and content strategies that scale products.";
+  "I bridge engineering and marketing teams, building tools and content strategies that scale products and drive adoption.";
