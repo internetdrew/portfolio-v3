23d5389b666f5833c9a04f9daac5b304bc7a0391
Andrew Rowley <andrew@internetdrew.com>
2025-07-24 15:00:56 -0400

    update description

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I tell stories that grab attention and build tools that actually help. Part marketer, part maker. Always focused on what people really need.";
+  "I build thoughtful products that help people work with less stress and create content experiences that help brands earn a meaningful place in people's lives.";
