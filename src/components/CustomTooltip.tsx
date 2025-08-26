c2811bab3f1eb0464690b80f39318fa633152ba6
Andrew Rowley <andrew@internetdrew.com>
2025-08-26 16:52:47 -0400

    add tooltips

---
+++ src/components/CustomTooltip.tsx
@@ -0,0 +1,24 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const CustomTooltip = ({
+  children,
+  content,
+}: {
+  children: React.ReactNode;
+  content: React.ReactNode;
+}) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        {children}
+        <TooltipContent>{content}</TooltipContent>
+      </TooltipTrigger>
+    </Tooltip>
+  );
+};
+
+export default CustomTooltip;
