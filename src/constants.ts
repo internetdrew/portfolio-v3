d7b3f16c185bf4fd2d660e2a1135e1ff22503c37
Andrew Rowley <andrew@internetdrew.com>
2025-08-27 14:28:23 -0400

    update description

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I love building web apps that help businesses operate efficiently and creative social experiences that build brand recognition, trust, and differentiation.";
+  "I love building web apps that help businesses operate efficiently and growing brand recognition, trust, and differentiation through creative social experiences.";
