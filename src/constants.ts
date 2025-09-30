be26d9a6cca1eedfb6f7649718cce98381d772bd
Andrew Rowley <andrew@internetdrew.com>
2025-09-30 12:11:58 -0400

    update description

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const siteDescription =
-  "I build apps that make work smarter and customer experiences better, and I craft stories and experiences that help brands stand out and be remembered.";
+  "I build apps that make work easier and customer experiences better, and I craft stories and experiences that help brands stand out and be remembered.";
