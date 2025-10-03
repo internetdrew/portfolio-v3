d00081eee56008ef1abd03ceb2d3bf4b0fd17efd
Andrew Rowley <andrew@internetdrew.com>
2025-10-03 12:00:00 -0400

    remove tooltips

--- src/components/EmailSonner.tsx
+++ src/components/EmailSonner.tsx
@@ -23,26 +23,21 @@ const EmailSonner = () => {
       onClick={copyEmailToClipboard}
       className="relative flex items-center text-muted-foreground gap-1 group"
     >
-      <Tooltip>
-        <TooltipTrigger>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="cursor-pointer size-5 transition-colors duration-150 group-hover:text-pink-600"
-          >
-            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"></path>
-            <rect x="2" y="4" width="20" height="16" rx="2"></rect>
-          </svg>
-          <TooltipContent>Email</TooltipContent>
-        </TooltipTrigger>
-      </Tooltip>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="cursor-pointer size-5 transition-colors duration-150 group-hover:text-pink-600"
+      >
+        <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"></path>
+        <rect x="2" y="4" width="20" height="16" rx="2"></rect>
+      </svg>
     </div>
   );
 };
