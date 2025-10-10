7dedcd341206c3d39bd47baf90d8116d4285090f
Andrew Rowley <andrew@internetdrew.com>
2025-10-10 09:50:32 -0400

    add guides to collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,18 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const insights = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/insights" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/collections/insights" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    ogImageSrc: z.string(),
+    isDraft: z.boolean(),
+  }),
+});
+
+const guides = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/collections/guides" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -14,4 +25,5 @@ const insights = defineCollection({
 
 export const collections = {
   insights,
+  guides,
 };
