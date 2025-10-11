ef0a2b251827dbdd5079de206e6ff1866f455ae5
Andrew Rowley <andrew@internetdrew.com>
2025-10-11 12:18:17 -0400

    specifically call out helping developers

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const siteDescription =
-  "I build web apps and help people understand and adopt technical products.";
+  "I build web apps and help developers understand and adopt technical products.";
 
 export const roles = "Web Developer. Technical Content Creator.";
